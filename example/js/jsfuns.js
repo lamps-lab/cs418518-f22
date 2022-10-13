aec8fafc0ba68c71c9ca6ba8b297c222e1c3190e
Jian Wu <56421035+lamps-director@users.noreply.github.com>
2022-10-13 09:56:27 -0400

    Add files via upload

---
+++ example/js/jsfuns.js
@@ -0,0 +1,9 @@
+function changeText() {
+	var span = document.getElementById("output");
+	var textBox = document.getElementById("textbox");
+        
+        textbox.style.fontSize = "33pt";
+   	textbox.style.fontFamily = "Comic Sans MS";
+	textbox.style.color = "green"; 
+}
+
